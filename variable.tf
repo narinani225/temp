aa07785aef02b966c785980e27a6971ce3981e70
narinani225 <100519638+narinani225@users.noreply.github.com>
2025-04-11 12:51:21 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,49 @@
+variable "resourcegroup" {
+  type = string
+}
+variable "location" {
+  type = string
+}
+variable "vnetname" {
+  type = string
+}
+variable "subnetname" {
+  type = string
+}
+variable "publicipname" {
+  type = string
+}
+variable "nsgname" {
+  type = string
+}
+variable "nicname" {
+  type = string
+}
+variable "vmname" {
+  type = string
+}
+variable "adminusername" {
+  type = string
+}
+variable "adminpassword" {
+  type = string
+}
+variable "customscriptname" {
+  type = string
+}
+variable "subscription_id" {
+  
+}
+variable "tenant_id" {
+  
+}
+variable "imagename" {
+  type = string
+}
+
+variable "galleryname" {
+  type = string
+}
+variable "imageresgrp" {
+  type = string
+}
